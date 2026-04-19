e81ad11695f098c897fa7d7feebc7ee629ee8de2
Seithreaper <96362685+Seithreaper@users.noreply.github.com>
2026-04-19 09:33:01 -0500

    Add productIndex with heat shield products

---
+++ src/data/productIndex.js
@@ -0,0 +1,25 @@
+const productIndex = [
+  {
+    number: "HS100",
+    code: "HS-100",
+    name: "Heat Shield Standard",
+    coverageSqFtPerGallon: 100,
+    containers: [1, 5]
+  },
+  {
+    number: "HS200",
+    code: "HS-200",
+    name: "Heat Shield High Build",
+    coverageSqFtPerGallon: 80,
+    containers: [1, 5]
+  },
+  {
+    number: "HS300",
+    code: "HS-300",
+    name: "Heat Shield Industrial",
+    coverageSqFtPerGallon: 120,
+    containers: [1, 5]
+  }
+]
+
+export default productIndex
